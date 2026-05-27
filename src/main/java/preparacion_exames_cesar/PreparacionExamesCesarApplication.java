f3774139d78135373396f14604010167776e7195
ainhoa <ainhoamiret@gmail.com>
2026-05-27 22:09:20 +0200

    Initial commit

---
+++ src/main/java/preparacion_exames_cesar/PreparacionExamesCesarApplication.java
@@ -0,0 +1,13 @@
+package preparacion_exames_cesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparacionExamesCesarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreparacionExamesCesarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/preparacion_exames_cesar/PreparacionExamesCesarApplicationTests.java
@@ -0,0 +1,13 @@
+package preparacion_exames_cesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreparacionExamesCesarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
